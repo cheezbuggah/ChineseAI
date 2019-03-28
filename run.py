b6bb59a60d489a66160b8d396abfa07d6aa9540a
Sjoerd van der Heide <sjoerd.vd.heide@hotmail.com>
2019-03-28 20:16:33 +0100

    Return probability list
    The predict function has an added functionality now; it returns a list of all characters and the probabilities associated with them

--- run.py
+++ run.py
@@ -2,9 +2,14 @@ import tensorflow as tf
 from flask import Flask, render_template, request, jsonify
 from keras.models import model_from_json, load_model
 from scipy.misc import imread, imresize
+import math
 import numpy as np
+import decimal
 import pandas
-import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('Agg')
+# import matplotlib.pyplot as plt
+import pandas as pd
 import time
 import keras
 import cv2 as cv
@@ -37,23 +42,24 @@ sys.path.append(os.path.abspath("./model"))
 UPLOAD = "./upload/"
 SAVE = "./saved/"
 BLACK = [0, 0, 0]
-
+class_labels_k49 = pd.read_csv("./Dataset/k49_classmap.csv").to_dict()
+print(class_labels_k49)
 app = Flask(__name__)
 
 global model, graph
 model, graph = init()
 
 
-def safesave(img, chr):
-	rst = random.randint(100000000, 999999999)
-	rst = str(rst)
-	try:
-		cv.imread(SAVE+rst+"_"+chr+".png", 0)
-		safesave(img, chr)
-		return
-	except:
-		cv.imwrite(SAVE+rst+"_"+chr+".png", img)
-		return
+# def safesave(img, chr):
+# 	rst = random.randint(100000000, 999999999)
+# 	rst = str(rst)
+# 	try:
+# 		cv.imread(SAVE+rst+"_"+chr+".png", 0)
+# 		safesave(img, chr)
+# 		return
+# 	except:
+# 		cv.imwrite(SAVE+rst+"_"+chr+".png", img)
+# 		return
 
 def predict(url):
 	img = cv.imread(url, 0)
@@ -72,36 +78,31 @@ def predict(url):
 			mh = h+y
 	cut = img[my:mh, mx:mw]
 	cut = cv.copyMakeBorder(cut, 10, 10, 10, 10, cv.BORDER_CONSTANT, value=BLACK)
-	plt.subplot(141), plt.imshow(cut, 'gray')
-	plt.subplot(142), plt.imshow(img, 'gray')
-	plt.show()
+	# plt.subplot(141), plt.imshow(cut, 'gray')
+	# plt.subplot(142), plt.imshow(img, 'gray')
+	# plt.show()
 	cut = imresize(cut, (28, 28))
 	cut2 = cut.reshape((1, 28, 28, 1))
 	cut2 = cut2.astype('float32')
 	cut2 /= 255
 
-
 	with graph.as_default():
 		out = model.predict(cut2)
 		print(out)
-		response = np.argmax(out, axis=1)
-		print(response)
-		prediction = str(response[0])
-
-	safesave(cut, prediction)
-	return prediction
+	return out
 
 
 
 @app.route('/process', methods=['POST'])
 def process():
+	prediction = []
 	imgdata = request.form['imgBase64']
 	if imgdata != None:
 		imgstr = re.search(r'base64,(.*)', str(imgdata)).group(1)
 		with open(UPLOAD+'output.png', 'wb') as output:
 			output.write(base64.b64decode(imgstr))
-		predict(UPLOAD+'output.png')
-	return jsonify({})
+		prediction = predict(UPLOAD+'output.png')
+	return jsonify({'rarray':prediction.tolist()})
 
 @app.route('/')
 def index():
